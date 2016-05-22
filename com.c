9b571ee336fa9996723397f98a192c1841f20251
kgimhg <kgimhg@naver.com>
2016-05-22 19:55:54 +0900

    Create com.c

---
+++ com.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void comma(int x, int y);
+int main()
+{
+    int var;
+    scanf("%d", &var);
+    comma(var, 0);
+    return 0;
+}
+void comma(int x, int y)
+{
+    if(x>0)
+    {
+        comma(x/10, y+1);
+        printf("%d", x%10);
+        if(y%3==0)
+            printf(",");
+    }
+}
