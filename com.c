e899ba8c74e0c2a35c4b1e99e7c62e4118b87397
kgimhg <kgimhg@naver.com>
2016-05-29 15:59:13 +0900

    Update com.c

--- com.c
+++ com.c
@@ -1,20 +1,35 @@
 #include<stdio.h>
-void comma(int x, int y);
 int main()
 {
-    int var;
-    scanf("%d", &var);
-    comma(var, 0);
-    return 0;
-}
-void comma(int x, int y)
-{
-    if(x>0)
-    {
-        comma(x/10, y+1);
-        printf("%d", x%10);
-        if(y!=0)    
-            if(y%3==0)
-                printf(",");
-    }
+ int k=0, a, i, n, x;
+ char var[50];
+ for(i=0;i<50;i++)
+ {
+     scanf("%c", &var[i]);
+     if(var[i]>=48&&var[i]<=57)
+     {
+         var[i] -= 48;
+         n++;
+     }
+     else if((var[i]=getchar())==10)
+        break;
+ }
+ for(i=0;i<n;i++)
+     k += sizeof(var[i]);
+ if(n%3==0)
+     a=0;
+     else if(n%3==1)
+     a=2;
+ else if(n%3==2)
+     a=1;
+ for(i=0;i<n;i++)
+ {
+     printf("%d", var[i]);
+     x++;
+     if((a+x)==n)
+         break;
+     if((a+x)%3 == 0)
+         printf(",");
+     }
+return 0;
 }
